ad4c160f9820896e2bea700c1c2c56a61b6759f0
Bernard Czenkusz <bernie@skipole.co.uk>
2025-10-27 19:10:30 +0000

    working on system setup

---
+++ indipyweb/web/setup.py
@@ -0,0 +1,53 @@
+"""
+Handles all routes beneath /setup
+"""
+
+
+from litestar import Litestar, get, post, Request, Router
+from litestar.plugins.htmx import HTMXTemplate, ClientRedirect
+from litestar.response import Template, Redirect
+from litestar.datastructures import State
+
+from litestar.response import ServerSentEvent, ServerSentEventMessage
+
+from . import userdata
+
+
+def logout(request: Request[str, str, State]) -> ClientRedirect|Redirect:
+    "Logs the session out and redirects to the login page"
+    if 'token' in request.cookies:
+        # log the user out
+        userdata.logout(request.cookies['token'])
+    if request.htmx:
+        return ClientRedirect("/login")
+    return Redirect("/login")
+
+
+@get("/")
+async def setup(request: Request[str, str, State]) -> Template:
+    "Get the setup page"
+    if request.auth != "admin":
+        return logout(request)
+    # get parameters from database and set up in context
+    context = {}
+    return Template(template_name="setup/setuppage.html", context=context)
+
+
+@get("/backupdb")
+async def backupdb(request: Request[str, str, State]) -> Template|Redirect:
+    """This creates a backup file of the user database"""
+    if request.auth != "admin":
+        return logout(request)
+    # userdata.dbbackup() actuall does the work
+    filename = userdata.dbbackup()
+    if filename:
+        return HTMXTemplate(None,
+                        template_str=f"<p id=\"backupfile\" style=\"color:green\" class=\"w3-animate-right\">Backup file created: {filename}</p>")
+    return HTMXTemplate(None,
+                        template_str="<p id=\"backupfile\"  style=\"color:red\" class=\"w3-animate-right\">Backup failed!</p>")
+
+
+
+setup_router = Router(path="/setup", route_handlers=[setup,
+                                                     backupdb,
+                                                    ])
